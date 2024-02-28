64927ae3fc98e7c818e911c48269f29b351303b2
mattemutti <mattemutti@gmail.com>
2024-02-28 16:00:10 +0100

    add number in cells and css .box aligne

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,28 @@
 
 const containerEl = document.getElementById('container'); //dichiaro la variabile associandola al container in html
 console.log(containerEl);
- let numCell;
-
-const maxCellsNumber = 100; // dichiariamo una costante con il numero di celle che vogliamo creare
-const markUp = `<div class="box"></div>` // dichiariamo la variabile con l'oggettoi html che vogliamo aggiungere
 
 
+//let numCell;
 
-
+const maxCellsNumber = 101; // dichiariamo una costante con il numero di celle che vogliamo creare
+ 
 
 
 
 
 // - richiamiamo da js il container e gli aggiungiamo al suo interno 100 div con la classe .box stilizzata in css
+// - ogni div dovrà essere numerato progressivamente con un ciclo for, lo stesso che creerà i div.
 
-for (let i = 0; i < maxCellsNumber; i++) {
 
-	numCell = i + 1;
-	console.log(typeof numCell, numCell);
-
+for (let i = 1; i < maxCellsNumber; i++) {
+	
+	const markUp = `<div class="box">${i}</div>` // dichiariamo la variabile con l'oggetto html che vogliamo aggiungere e all'interno il numero corrispondente
 	containerEl.insertAdjacentHTML('beforeend', markUp) //ogni volta che cicla crea un markUp al .containerprima della fine
-
+	
 }
 
 
-// - ogni div dovrà essere numerato progressivamente con un ciclo for, lo stesso che creerà i div.
 
 // let boxEl = document.getElementsByClassName("box")
 // console.log(boxEl);
