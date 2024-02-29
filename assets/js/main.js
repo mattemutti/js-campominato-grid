4dd5fca69c1b484048b68bd575f51275eb67e064
mattemutti <mattemutti@gmail.com>
2024-02-29 13:22:28 +0100

    update check number random error

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,38 +8,38 @@ const containerEl = document.getElementById('container'); //dichiaro la variabil
 const maxCellsNumber = 101; // dichiariamo una costante con il numero di celle che vogliamo creare
 const startPlay = document.getElementById('btn_start')
 
-startPlay.addEventListener('click', function(){
+startPlay.addEventListener('click', function () {
 
 	// cancelliamo un eventuale partita già iniziata
 	// bisogna cancellare eventuali div con class="box" in pagina
 
 	containerEl.innerHTML = ""
 
-	
+
 	// - richiamiamo da js il container e gli aggiungiamo al suo interno 100 div con la classe .box stilizzata in css
 	// - ogni div dovrà essere numerato progressivamente con un ciclo for, lo stesso che creerà i div.
-	
+
 	for (let i = 1; i < maxCellsNumber; i++) {
-	
+
 		const markUp = `<div class="box">${i}</div>` // dichiariamo la variabile con l'oggetto html che vogliamo aggiungere e all'interno il numero corrispondente
 		containerEl.insertAdjacentHTML('beforeend', markUp) //ogni volta che cicla crea un markUp al .containerprima della fine
-	
+
 	}
-	
+
 	// - creare una regola css che colora il background della cella di azzurro quando clicchiamo dulla casella e
 	// dia in console log il numero della casella cliccata
 	// - quando clicchiamo sulla cella, questa dovrà colorarsi di azzuro e stampare il numero corrispondente in console
-	
+
 	const boxElements = document.getElementsByClassName('box')
 	// console.log(boxEl);
-	
-	for (let i = 0; i < boxElements.length; i++) {   
+
+	for (let i = 0; i < boxElements.length; i++) {
 		const element = boxElements[i];
 		//console.log(element);
-	
+
 		element.addEventListener('click', function (e) {
-	
-			element.style.backgroundColor = 'azure'
+
+			element.style.backgroundColor = 'blue'
 			numClickUser = this.innerHTML;					//variabile che contiene il numero di casella cliccata dall'utente
 			console.log(numClickUser);
 
@@ -48,3 +48,65 @@ startPlay.addEventListener('click', function(){
 })
 
 
+
+
+
+
+// - Creare un'array vuoto
+const numbMushrooms = [];
+console.log(numbMushrooms);
+// - Con un ciclo generiamo tramite Math.random, 16 numeri (min 1 max 16) che però devono essere diversi tra loro e li salviamo pushandoli  all'interno dell'array
+
+console.log();
+/**
+ * crea un numero random da 1 a max
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+/*
+for (let i = 0; i < 16; i++) {
+	const element = numbMushrooms[i];
+	console.log(element);
+
+	 // calcolo un numero randomico e lo inserisco nell'array
+	
+}
+*/
+
+let numbArray = true
+
+while (numbArray) {
+	
+	let numbRand = randomNumber(1, 16);
+
+	if (numbRand != numbMushrooms && numbMushrooms.length <= 15) {
+		numbMushrooms.push(numbRand);
+	} else {
+
+		numbArray = false;
+	}
+
+}
+
+console.log(numbMushrooms);
+
+
+// - cicliamo all'interno dei numeri delle celle ed al numero corrispondente inseriamo nella casella un emmoticon con innerHTML
+
+
+
+for (let i = 0; i < numbMushrooms.length; i++) {
+	const element = numbMushrooms[i];
+
+	console.log(element);
+	if (element != ) {
+		
+	}
+	
+}
--- readme.md
+++ readme.md
@@ -87,3 +87,49 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 
+
+_______ steps Fabio
+
+# Milestone 1
+// Generare massimo 16 funghi in questi range relativi al livello scelto // 100 -> min: 1 max: 100 | Totale funghi 16 // 81 -> min: 1 max: 81 | Totale funghi 16 // 49 -> min: 1 max: 49 | Totale funghi 16
+
+Attenzione: nella stessa cella può essere posizionato al massimo un fungo, perciò nell’array dei funghi non potranno esserci due numeri uguali.
+
+Steps
+
+create a random numbers function to generate a number between a min and max (using the range 1-100 | 1-81 | 1-49)
+create an empty mushroomsArray
+
+(while) loop over until the empty array has 16 elements (not repeated)
+check if the generated random number is included in the mushroomsArray
+if not push it inside the array
+
+ripete il ciclo finché l'array vuoto non ha 16 elementi (non ripetuti)
+controlla se il numero casuale generato è incluso nell'array
+in caso contrario spingerlo all'interno dell'array
+ 
+
+
+# Milestone 2
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una fungo - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+Steps
+
+read the number of the clicked cell
+check if the clicked cell's number is included inside the mushrooms array
+IF true
+paint the cell to red
+end the game console.log('end game')
+ELSE
+paint the cell to blue
+Milestone 3
+La partita termina quando il giocatore clicca su una fungo o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono funghi).
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una fungo.
+
+Steps
+
+keep the count of the clicked cells
+end the game
+quando il numbero di celle cliccate é uguale al numero di celle totali meno il numero dei funghi hai vinto il gioco.
+prevent the user from clicking again after the game is over
